c9bd383f513e4e35815143fd3553d41f043c8002
claudioSilva11 <99560963+claudioSilva11@users.noreply.github.com>
2022-06-22 12:47:27 -0300

    Add files via upload
    Projeto final de POO 2022

--- Conexao.java
+++ Conexao.java
@@ -9,9 +9,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+
 /**
  *
- * @author Carla
+ * @author Cláudio
  */
 public class Conexao {
     
@@ -31,24 +32,102 @@ public class Conexao {
 
         return conn;
     }
+    public void todosUsuarios(){      // ver todos os usuário no banco de dados
+        try (Connection conexao = connect();
+                Statement stmt=conexao.createStatement()){
+                                try {
+                                stmt.executeQuery("(SELECT * FROM usuarios) ");
+                                } catch
+                                        (SQLException e ) {
+                                                                throw new Error("Problema insert", e);
+                                        
+                                }
+                        } catch (SQLException e) {
+                                        throw new Error("Problema na conexão", e);
+                                        }
+                        }
    
-    public void cadastroCliente(/*Conta cliente/* */){
+    public void cadastroCliente(){      // Insere o novo usuário no banco de dados
             try (Connection conexao = connect();
                     Statement stmt=conexao.createStatement()){
                                     try {
-                                    stmt.executeQuery("insert into usuario (tipoconta, nome, sobrenome, senha, cheque, cpf) values (?, ?, ?, ?, ?,?)");
+                                    stmt.executeQuery("INSERT INTO usuarios(tipo_conta, nome, sobrenome, senha, cheque, cpf, tipo_usuario) VALUES (?, ?, ?, ?, ?,?, ?)");
                                     } catch
                                             (SQLException e ) {
                                                                     throw new Error("Problema insert", e);
-                                            //TODO: handle exception
+                                            
                                     }
                             } catch (SQLException e) {
                                             throw new Error("Problema na conexão", e);
                                             }
                             }
 
+    public void login(){          // Faz login caso o usuário seja cadastrado
+        try (Connection conexao = connect();
+                Statement stmt=conexao.createStatement()){
+                                try {
+                                stmt.executeQuery("(SELECT * FROM usuarios WHERE login=? and senha=?)");
+                                
+                                } catch
+                                        (SQLException e ) {
+                                                                throw new Error("Problema no select", e);
+                                        
+                                }
+                        } catch (SQLException e) {
+                                        throw new Error("Problema na conexão", e);
+                                        }
+                        }
+
+
+    public void extrato(){   // Retorna o extrato do procedimento
+            try (Connection conexao = connect();
+                    Statement stmt=conexao.createStatement()){
+                                    try {
+                                    stmt.executeQuery("(SELECT * FROM extrato)");
+                                    } catch
+                                            (SQLException e ) {
+                                                                    throw new Error("Problema no select", e);
+                                            
+                                    }
+                            } catch (SQLException e) {
+                                            throw new Error("Problema na conexão", e);
+                                            }
+                            }
+        public void consultarContasCorrente(){ // Essa função me retornara os nomes dos usuários que possuem conta... 
+                try (Connection conexao = connect();  // corrente e o seu saldo
+                Statement stmt=conexao.createStatement()){
+                                try {
+                                stmt.executeQuery("(SELECT usuarios.nome, conta_corrente.saldo from usuarios, conta_corrente where usuarios.id=conta_corrente.id;)");
+                                } catch
+                                        (SQLException e ) {
+                                                                throw new Error("Problema no select", e);
+                                        
+                                }
+                        } catch (SQLException e) {
+                                        throw new Error("Problema na conexão", e);
+                                        }
+                        }
+
+public void consultarContasPoupanca(){  // Essa função me retornara os nomes dos usuários que possuem conta... 
+        try (Connection conexao = connect();  // poupança e o seu saldo
+                Statement stmt=conexao.createStatement()){
+                                try {
+                                stmt.executeQuery("(SELECT usuarios.nome, conta_poupanca.saldo from usuarios, conta_poupanca where usuarios.id=conta_poupanca.id;)");
+                                } catch
+                                        (SQLException e ) {
+                                                                throw new Error("Problema no select", e);
+                                        
+                                }
+                        } catch (SQLException e) {
+                                        throw new Error("Problema na conexão", e);
+                                        }
+                        }
+                        
     public static void main(String[] args) {
-        // TODO code application logic here
+        Conexao bd= new Conexao();
+        bd.cadastroCliente();
+        
     }
     
 }
+
